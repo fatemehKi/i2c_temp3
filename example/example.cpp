85dddac80034c4e42004647991a53c7d6ea0bd83
fatemehKi <40069260+fatemehKi@users.noreply.github.com>
2018-07-03 09:36:51 -0400

    Update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -10,16 +10,14 @@
 #include <string.h>
 #include <termios.h>
 #include <time.h>
+#include <AdafruitMCP9808.h>
 
-#include "AdafruitMCP9808.h"
-
-/*
 int getkey() {
     int character;
     struct termios orig_term_attr;
     struct termios new_term_attr;
 
-    /* set the terminal to raw mode
+    /* set the terminal to raw mode */
     tcgetattr(fileno(stdin), &orig_term_attr);
     memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
     new_term_attr.c_lflag &= ~(ECHO|ICANON);
@@ -27,16 +25,16 @@ int getkey() {
     new_term_attr.c_cc[VMIN] = 0;
     tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
 
-    /* read a character from the stdin stream without blocking
-    /*   returns EOF (-1) if no character is available
+    /* read a character from the stdin stream without blocking */
+    /*   returns EOF (-1) if no character is available */
     character = fgetc(stdin);
 
-    /* restore the original terminal attributes
+    /* restore the original terminal attributes */
     tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
 
     return character;
 }
-*/
+
 int main() {
     AdafruitMCP9808 *adafruitMCP9808 = new AdafruitMCP9808() ;
     int err = adafruitMCP9808->openAdafruitMCP9808();
